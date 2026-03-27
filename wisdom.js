be4b802eb6a5d4ecf61576cb89d0629cad751703
Phludge Dredd <a@oldhacker.org>
2026-03-27 00:44:26 -0400

    Wisdom.js
    Added random quotes to interests page headline.

---
+++ wisdom.js
@@ -0,0 +1,89 @@
+
+var quotes=new Array()
+
+//quotes list.
+
+quotes[0]='All generalizations are false, including this one.'
+
+quotes[1]='To hell with circumstances; I create opportunities. <b>—Bruce Lee</b>'
+
+quotes[2]='Science without religion is lame, religion without science is blind. <b>—Albert Einstein</b>'
+
+quotes[3]='Does the process of observation create reality?'
+
+quotes[4]='All events, before a measurement is made, are possible.'
+
+quotes[5]='Individual minds are whirl pools in the stream of consciousness.'
+
+quotes[6]='Within the mesh of cosmic entanglement.'
+
+quotes[7]='Counterintuitive thinking.'
+
+quotes[8]='Artificial Intelligence usually beats real stupidity.'
+
+quotes[9]='Make it idiot proof and someone will make a better idiot.'
+
+quotes[10]='Dream is a higher-order spatial dimension'
+
+quotes[11]='Runes were upon his tongue, As on the warrior\'s sword. <b>—Longfellow</b>'
+
+quotes[12]='Stop and smell the overclocked silicon.'
+
+quotes[13]='I am the universe experiencing itself through itself.'
+
+quotes[14]='My belief is a working theory that\'s always being revised.'
+
+quotes[15]='Society often forgives the criminal; it seldom forgives the dreamer.'
+
+quotes[16]='Consciousness: that annoying time between naps.'
+
+quotes[17]='You are just a fluctuation that got complicated enough to wonder what\'s it made of <b>—Richard Feynman</b>'
+
+quotes[18]='Hackers created the modern world, a world in which we are now branded as outlaws.'
+
+quotes[19]='It takes twenty years or more of peace to make a man; it takes only twenty seconds of war to destroy him.'
+
+quotes[20]='To talk without thinking is to shoot without aiming.'
+
+quotes[21]='A man is a god in ruins. <b>—Ralph Waldo Emerson</b>'
+
+quotes[22]='Power without awareness is chaos, power with awareness is creation.'
+
+quotes[23]='Good judgement is like good taste, it\'s usually few and far between.'
+
+quotes[24]='The Universe is a fractal hologram of interwoven realities.'
+
+quotes[25]='A difference in measure is a difference in perspective.'
+
+quotes[26]='Every new beginning comes from some other beginning\'s end. <b>—Seneca</b>'
+
+quotes[27]='I shun the common, and celebrate the weird.'
+
+quotes[28]='Within every nonsense there is sense.'
+
+quotes[29]='All matter is simply undulations in the fabric of space. <b>—William Clifford</b>'
+
+quotes[30]='Act as if what you do makes a difference. It does. <b>—William James</b>'
+
+quotes[31]='I finally started thinking outside the box, only to find myself inside a larger box. <b>—Robert H. Calloway</b>'
+
+quotes[32]='There is no reality beyond the reality we\'re creating. <b>—Michael Talbot</b>'
+
+quotes[33]='All that’s visible springs from causes intimate to you. While walking, sitting, lying down, the body itself is complete truth. <b>—Dogen</b>'
+
+quotes[34]='Learn to watch your drama unfold while at the same time knowing you are more than your drama. <b>—Ram Dass</b>'
+
+quotes[35]='We are in the universe and the universe is in us. <b>—Neil deGrasse Tyson</b>'
+
+quotes[36]='In all chaos there is a cosmos, in all disorder a secret order. <b>—Carl Gustav Jung</b>'
+
+quotes[37]='Matter and Energy is exchangeable, not distinct. <b>—Albert Einstein</b>'
+
+quotes[38]='Omnia ab Uno - Everything originates from the One.'
+
+quotes[39]='Ex Centro In Centrum - Everything originates from one center and returns to that center.'
+
+quotes[40]='Nature is neutral. It\'s people that see black or white; only <b>this way</b> or <b>that way</b>, without realizing what they perceive is illusory.'
+
+var whichquote=Math.floor(Math.random()*(quotes.length))
+document.write(quotes[whichquote])
