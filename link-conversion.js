003f0aff1cabdf9a9f930523a8d7988e8d384ab4
Phludge Dredd <a@oldhacker.org>
2026-06-30 15:43:00 -0400

    Update link-conversion.js
    email change

--- link-conversion.js
+++ link-conversion.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', function ()
 	const a = document.getElementById('link-conversion');
 
 	a.setAttribute('href', a.getAttribute('href')
-		.replace('-', 'lan@')
+		.replace('-', 'a@')
 		.replaceAll('-', '.')
-		.replace('/', 'falsedoor.com')
-		.replace('to', 'mailto:a')
+		.replace('/', 'oldhacker.org')
+		.replace('to', 'mailto:')
 	);
 });
